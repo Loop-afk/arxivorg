2be53291f38a9dffd60845b492b45b021d930abe
DJOUDI Ferial <ferial.djoudi@etu.univ-amu.fr>
2020-03-29 19:46:25 +0200

    Ajouter une boite de dialogue pour le telechargement d article

--- src/main/java/app/arxivorg/ArxivOrg.java
+++ src/main/java/app/arxivorg/ArxivOrg.java
@@ -1,13 +1,36 @@
 package app.arxivorg;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceDialog;
 import javafx.stage.Stage;
+
+import java.util.Optional;
 
 
 public class ArxivOrg extends Application {
+
+    @FXML
+    private Button downloadButton ;
+
+    // Afficher la boite de dialogue de téléchargement
+    private void showDialogueWin (){
+        String [] choices = {"Article sélectionné", "Tout les articles"};
+        ChoiceDialog<String> cDial = new ChoiceDialog<>(choices[0], choices);
+        cDial.setTitle("Téléchargement");
+        cDial.setHeaderText("Sélectionner votre choix :");
+        cDial.setContentText("Choix : ");
+
+        Optional<String> selection = cDial.showAndWait();
+        selection.ifPresent(str -> System.out.println("Selection" + str));
+    }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -15,6 +38,13 @@ public class ArxivOrg extends Application {
         primaryStage.setTitle("Main Application");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
+        downloadButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                showDialogueWin();
+            }
+        });
     }
 
     public static void main(String[] args) { launch(args); }
--- src/main/java/app/arxivorg/controller/ArxivOrgController.java
+++ src/main/java/app/arxivorg/controller/ArxivOrgController.java
@@ -5,7 +5,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
@@ -13,29 +12,17 @@ import java.util.ResourceBundle;
 
 public class ArxivOrgController implements Initializable {
 
-
     @FXML private ListView<Article> shortListView;
-//    @FXML private ComboBox<String> cbxCategories;
     ObservableList<Article> names = FXCollections.observableArrayList(Article.infos);
 
-
-
-    //    @Override
     public void initialize(URL location, ResourceBundle resourceBundle) {
         setShortListView();
-//        setCbxCategories();
     }
-
 
     @FXML
     private void setShortListView(){
         shortListView.refresh();
         shortListView.getItems().addAll(names);
     }
-
-/*    @FXML
-    private void setCbxCategories(){
-        cbxCategories.getItems().addAll(Article.getAllCategories(Article.infos));
-        }*/
 
 }
