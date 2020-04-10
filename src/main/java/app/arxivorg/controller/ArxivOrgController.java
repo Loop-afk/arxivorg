8eb1d0c67d6d5f24601c989c4acf892d8eb87e3c
DJOUDI Ferial <ferial.djoudi@etu.univ-amu.fr>
2020-04-10 11:48:46 +0200

    ajouter implémentation téléchargement articles 'en cours'

--- src/main/java/app/arxivorg/controller/ArxivOrgController.java
+++ src/main/java/app/arxivorg/controller/ArxivOrgController.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 
@@ -15,13 +16,15 @@ public class ArxivOrgController implements Initializable {
 
     @FXML private ListView<Article> shortListView;
     @FXML private ComboBox<String> cbxCategories;
+    @FXML private Button downloadButton ;
+    private static int nClicks = 0;
 
     ObservableList<Article> names = FXCollections.observableArrayList(Article.infos);
 
     public void initialize(URL location, ResourceBundle resourceBundle) {
         setShortListView();
         setCbxCategories();
-
+        downloadArticles();
     }
 
     @FXML
@@ -33,6 +36,20 @@ public class ArxivOrgController implements Initializable {
     @FXML
     private void setCbxCategories(){
         cbxCategories.getItems().addAll(Article.getAllCategories(Article.infos));
+    }
+
+    @FXML
+    private void downloadArticles (){
+        /*
+            si je clique sur le bouton
+            => alors je télécharge les articles (sous formes PDF)
+        */
+        //  1er test : je clique => j'affiche un msg combien de fois j'ai cliqué :
+        downloadButton.setOnAction(event -> {
+            nClicks++;
+            System.out.println("Clicked " + nClicks + " times.");
+        });
+
     }
 
 }
