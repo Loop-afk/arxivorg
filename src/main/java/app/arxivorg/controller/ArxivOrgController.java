11b8d19a60cb66176ab184c46201e149514e0024
DJOUDI Ferial <ferial.djoudi@etu.univ-amu.fr>
2020-04-18 22:51:54 +0200

    ajouter l event : selectionner un élm dans la liste

--- src/main/java/app/arxivorg/controller/ArxivOrgController.java
+++ src/main/java/app/arxivorg/controller/ArxivOrgController.java
@@ -3,11 +3,14 @@ import app.arxivorg.model.Article;
 import javafx.animation.PauseTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 
 
@@ -15,6 +18,7 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 public class ArxivOrgController implements Initializable {
@@ -22,7 +26,6 @@ public class ArxivOrgController implements Initializable {
     @FXML private ListView<Article> shortListView;
     @FXML private ComboBox<String> cbxCategories;
     @FXML private Button downloadButton ;
-    private static int nClicks = 0;
 
     ObservableList<Article> names = FXCollections.observableArrayList(Article.infos);
 
@@ -30,12 +33,14 @@ public class ArxivOrgController implements Initializable {
         setShortListView();
         setCbxCategories();
         downloadArticles();
+       // showInfosArticles();
     }
 
     @FXML
     private void setShortListView(){
         shortListView.refresh();
         shortListView.getItems().addAll(names);
+        shortListView.addEventFilter(MouseEvent.MOUSE_PRESSED, mouseEvent -> System.out.println(names));
     }
 
     @FXML
@@ -46,12 +51,9 @@ public class ArxivOrgController implements Initializable {
     @FXML
     private void downloadArticles (){
 
-        // 2m test : je clique => j'affiche la liste d'articles (que par la suite on va la télécharger)
 //            downloadButton.setOnAction(event -> {
-//                System.out.println("la liste d article : " + Article.infos);
+//                System.out.println("la liste d article : " + names);
 //            });
-
-        // 3m test : je télécharge la liste
 
             downloadButton.setOnAction(event -> {
                 try (BufferedInputStream inputStream = new BufferedInputStream(new URL("https://arxiv.org/pdf/2003.04748.pdf").openStream());
