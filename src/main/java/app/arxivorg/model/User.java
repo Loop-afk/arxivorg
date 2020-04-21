2f3cd3504d8c9ddff0231016e8c757ec732faf3d
Albouy <florent.albouy@etu.univ-amu.fr>
2020-04-21 16:39:42 +0200

    Mise en place d'une classe User et d'une classe ConnectUser pour gérer la connection Utilisateur ainsi que les favoris.

---
+++ src/main/java/app/arxivorg/model/User.java
@@ -0,0 +1,38 @@
+package app.arxivorg.model;
+
+import java.util.Date;
+import java.util.LinkedList;
+
+public class User {
+    private final String username;
+    private String password;
+    private final Date dateOfCreation;
+    private LinkedList<Article> favoris;
+
+    public User(String username, String password, Date dateOfCreation) {
+        this.username = username;
+        this.password = password;
+        this.dateOfCreation = dateOfCreation;
+        this.favoris = new LinkedList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Date getDateOfCreation() {
+        return dateOfCreation;
+    }
+
+    public LinkedList<Article> getFavoris() {
+        return favoris;
+    }
+
+    public void addArticle(Article article){
+        this.favoris.addLast(article);
+    }
+}
