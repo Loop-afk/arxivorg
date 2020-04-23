b0175391eba958bd0c8c6088df1a35f00784a114
DJOUDI Ferial <ferial.djoudi@etu.univ-amu.fr>
2020-04-23 12:31:50 +0200

    Test

--- src/main/java/app/arxivorg/controller/FavoritesController.java
+++ src/main/java/app/arxivorg/controller/FavoritesController.java
@@ -1,5 +1,25 @@
 package app.arxivorg.controller;
 import app.arxivorg.model.Article;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
-public class FavoritesController {
+import javax.swing.text.html.ListView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FavoritesController implements Initializable  {
+
+    @FXML
+    public ListView;
+    @FXML
+    public Button downloadButton;
+    @FXML
+    public Button deleteButton;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
