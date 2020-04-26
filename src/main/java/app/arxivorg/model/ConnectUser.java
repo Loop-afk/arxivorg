180bb22ea572e7e0d84f97fd6becbcb9941fae0b
Albouy <florent.albouy@etu.univ-amu.fr>
2020-04-26 10:58:45 +0200

    Ajout des méthodes permettant d'ajouter ou de modifier des données utilisateur sur le fichier json servant de mini banque de données (ajout nouvel utilisateur, ajout ou suppression d'un article en favoris).

--- src/main/java/app/arxivorg/model/ConnectUser.java
+++ src/main/java/app/arxivorg/model/ConnectUser.java
@@ -5,16 +5,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.LinkedList;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class ConnectUser {
-    public static JSONObject data = new JSONObject();
-
 
     //Fonction permettant de créer un fichier JSON qui contiendra tous les utilisateurs.
     public static void generateJsonFile() throws JSONException{
@@ -52,9 +47,9 @@ public class ConnectUser {
         }
     }
 
-    public static boolean checkUser(User user){
-        int result = 0;
-
+    //Fonction qui permet de vérifier si un utilisateur existe dans le fichier json et le retourne le cas échéant.
+    public static User checkUser(User user){
+        User result = null;
         //Ouverture du fichier Users.json.
         FileInputStream fs = null;
         try {
@@ -81,10 +76,11 @@ public class ConnectUser {
         if (tmp.get(user.getUsername()) != null) {
             User test = (User) tmp.get(user.getUsername());
             if (test.getUsername().equals(user.getUsername()) && test.getPassword().equals(user.getPassword())) {
-                result = 1;
+                result = test;
             }
         }
 
+        //Fermeture du fichier.
         try {
             fs.close();
         } catch(IOException e) {
@@ -93,14 +89,30 @@ public class ConnectUser {
             System.exit(-1);
         }
 
-        return result == 1;
+        return result;
     }
 
-    public void addUser(User user){
-        if (checkUser(user)) {
+    //Permet d'ajouter un nouvel utilisateur si ce dernier n'existe pas déjà dans le fichier json.
+    public static void addUser(User user) throws IOException {
+        if (checkUser(user) == null) {
+            //Permet de créer un writer qui va être capable d'ajouter des lignes dans un fichier.
+            FileWriter writer = new FileWriter("Users.json", StandardCharsets.UTF_8);
+
+            //Prépare le nouvel utilisateur devant être ajouté.
             User[] newUser = {new User(user.getUsername(), user.getPassword())};
             JSONObject json = new JSONObject();
             json.put("" + user.getUsername() + "", new JSONArray(newUser));
+
+            //Permet d'écrire à la fin du fichier le nouvel utilisateur.
+            writer.append(json.toString());
+        }
+        else {System.out.println("Veuillez choisir un autre nom d'utilisateur.");}
+    }
+
+    //Permet en cliquant sur un bouton "Ajouter" lié à un article de rajouter un article à la liste des favoris.
+    public void addFavorite(Article article, User user) throws IOException{
+            //Permet de créer un writer qui va être capable d'ajouter des lignes dans un fichier.
+            FileWriter writer = new FileWriter("Users.json", StandardCharsets.UTF_8);
 
             //Ouvre un lecteur de fichier.
             FileInputStream fs = null;
@@ -123,11 +135,59 @@ public class ConnectUser {
             stringJson = new StringBuilder(stringJson.toString().replaceAll("[\t ]", ""));
             String j = stringJson.toString();
 
-        }
-        else {System.out.println("Veuillez choisir un autre nom d'utilisateur.");}
+            //Transforme ce string en objet json pour avoir accès aux méthodes de recherche de contenu de la classe.
+            JSONObject json = new JSONObject(j);
+
+            //Ajout de l'article dans les favoris du bon utilisateur.
+            User toChange = (User) json.get(user.getUsername());
+            toChange.addArticle(article);
+
+            //Suppression des données anciennes et ajout des nouvelles avec le nouvel article.
+            json.remove(user.getUsername());
+            json.put("" + user.getUsername() + "", new JSONArray(toChange));
+
+            //Réécriture du fichier avec le changement.
+            writer.write(json.toString());
     }
 
-    public void addFavorite(Article article){
+    //Permet en cliquant sur un bouton "Supprimer" lié à un article d'enlever un article de la liste des favoris.
+    public void deleteFavorite(Article article, User user) throws IOException{
+        //Permet de créer un writer qui va être capable d'ajouter des lignes dans un fichier.
+        FileWriter writer = new FileWriter("Users.json", StandardCharsets.UTF_8);
 
+        //Ouvre un lecteur de fichier.
+        FileInputStream fs = null;
+        try {
+            fs = new FileInputStream("Users.json");
+        } catch (FileNotFoundException e) {
+            System.err.println("Fichier Users.json introuvable");
+            System.exit(-1);
+        }
+
+        //Ouvre un scanner pour lire le fichier Users.json.
+        StringBuilder stringJson = new StringBuilder();
+        Scanner scanner = new Scanner(fs);
+
+        //Construit un string dont le contenu représente celui du fichier JSON.
+        while (scanner.hasNext()) {
+            stringJson.append(scanner.nextLine());
+            scanner.close();
+        }
+        stringJson = new StringBuilder(stringJson.toString().replaceAll("[\t ]", ""));
+        String j = stringJson.toString();
+
+        //Transforme ce string en objet json pour avoir accès aux méthodes de recherche de contenu de la classe.
+        JSONObject json = new JSONObject(j);
+
+        //Suppression de l'article dans les favoris du bon utilisateur.
+        User toChange = (User) json.get(user.getUsername());
+        toChange.removeArticle(article);
+
+        //Suppression des données anciennes et ajout des nouvelles avec le nouvel article.
+        json.remove(user.getUsername());
+        json.put("" + user.getUsername() + "", new JSONArray(toChange));
+
+        //Réécriture du fichier avec le changement.
+        writer.write(json.toString());
     }
 }
