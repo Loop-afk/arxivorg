112a01df7c27bc8fb13d7fcaafa7dbac54f527ea
Kevin <kevin.pierrard@etu.univ-amu.fr>
2020-04-26 20:50:36 +0200

    Petit avancement sur la CLI

--- src/main/java/app/arxivorg/ArxivOrgCLI.java
+++ src/main/java/app/arxivorg/ArxivOrgCLI.java
@@ -9,25 +9,27 @@ import java.util.Scanner;
 public class ArxivOrgCLI extends Article {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the arXiv organizer!");
-        LinkedList<Article> listOfArticle = new LinkedList<>(Article.readFile(Article.getArticlesFromArXivWithLimitedNumber("Java", 5)));
+        LinkedList<Article> listOfArticle = new LinkedList<>(Article.readFile(Article.getArticlesFromArXivWithLimitedNumber("", 5)));
         //        System.out.println("You requested command '" + args[0] + "' with parameter '" + args[1] + "'");
         System.out.println("Input your command: ");
         Scanner scanner = new Scanner(System.in);
         String[] expression = scanner.next().split(" ");
         while(scanner.hasNext()) {
+
             if(expression[0].equals("list")){
                 switch (scanner.next()){
                     case "search":
                         // j'ai limité 10 ici le nombre de recher pour la simplicité de lecture des résultats
                         listOfArticle = readFile(getArticlesFromArXivWithLimitedNumber(scanner.next(), 10));
-                        System.out.println("La nouvelle list d'artcile est : \n"+listOfArticle);
+                        System.out.println("Liste actualisée, affichez là avec la commande: list show");
                         break;
                     case "-c":
-                        filterByCategory(listOfArticle, scanner.next());
-                        System.out.println(listOfArticle);
+                        LinkedList<Article> filteredByCategories;
+                        filteredByCategories = filterByCategory(listOfArticle, scanner.next());
+                        System.out.println(filteredByCategories);
                         break;
                     case "-a":
-                        System.out.println(getAllAuthors(listOfArticle));
+                        System.out.println(getAllArticles(scanner.next() + scanner.next()));
                         break;
                     case "exit":
                         System.out.println("Merci d'avoir utilisé l'invite de commande");
--- src/main/java/app/arxivorg/model/Main.java
+++ src/main/java/app/arxivorg/model/Main.java
@@ -7,13 +7,14 @@ import static app.arxivorg.model.Article.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-      LinkedList<Article> list1 = readFile(getArticlesFromArXivWithLimitedNumber("electron, impact",5));
+      LinkedList<Article> list1 = readFile(getArticlesFromArXivWithLimitedNumber("python",5));
       LinkedList<Article> list2 = readFile(getArticlesFromArXivWithLimitedNumber("electron, impact",5));
 
      /*   for(Article list: list1){
             System.out.println(list.getSummary());*/
+        System.out.println(getAllCategories(list1));
+        System.out.println(filterByCategory(list1, "cs.DS"));
 
-        System.out.println(list1.toString().contains(list2.get(0).getId()));
         }
     }
 
